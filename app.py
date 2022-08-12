ab8be6bd6639748382658fb71a1316c53cca73e9
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-12 16:14:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My Wonderful Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return {'message': 'Hello World', 'status_code': 200}
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+@app.route('/store')
+def get_stores():
+    pass
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
